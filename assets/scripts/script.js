19e5fdf7cf8ee2029fa938809167aab3afd99ed2
Lsubias <subias434@gmail.com>
2023-05-22 18:49:46 -0300

    termine el form, es a traves de consol.log

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,17 @@
-//recuperamos los datos del formulario
-document.addEventListener('DOMContentLoaded'),() =>{
-    const form = document.getElementById(formula);
+//tomamos el formulario desde javascript
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('formula');
+
+    form.addEventListener("submit", onformSubmit);
+});
+
+
+//levantamos el nombre, correo y texto con los id que le proveimos en html con javascript
+function onformSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const name = data.get("name");
+    const email = data.get("mail");
+    const text = data.get("msg");
+    console.log(`Nombre: ${name}, correo electrónico: ${email}, mensaje: ${text}`);
 }
-
-
-
-
-form.addEventListener("submit", onformSubmit);
-//funcion que ejecuta al enviar el formulario
-
-function onformSubmit(event){
-event.preventDefault();
-const data = new FormData(event.target);
-const name = data.get("name");
-const email = data.get("email");
-const text  = data.get("msg");
-console.log(`Nombre: ${name}, correo electronico: ${email}, mensaje: ${msg}`);
-}
-
