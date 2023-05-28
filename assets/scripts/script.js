d28a22408dc003d973c1c62c69974155f019c5b5
Lsubias <subias434@gmail.com>
2023-05-27 22:01:26 -0300

    se agrego la validacion del formulario

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,11 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener("submit", onformSubmit);
 });
 
+function validarFormulario(evento){
+evento.preventDefault();
+
+}
+
 function onformSubmit(event) {
   event.preventDefault();
   const data = new FormData(event.target);
   const name = data.get("name");
   const email = data.get("mail");
   const text = data.get("msg");
+  if(name.length == 0 || email.length ==0 ){
+    alert('No has escrito nada en el usuario');
+    return;
+  }
   console.log(`Nombre: ${name}, correo electrónico: ${email}, mensaje: ${text}`);
 }
